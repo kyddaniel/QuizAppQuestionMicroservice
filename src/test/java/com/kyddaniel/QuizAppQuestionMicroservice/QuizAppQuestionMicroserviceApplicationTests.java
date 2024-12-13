cb82fbb9ddae52c741b8c32f250069ac187bd4ff
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-13 22:19:39 +0800

    Initial commit

---
+++ src/test/java/com/kyddaniel/QuizAppQuestionMicroservice/QuizAppQuestionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyddaniel.QuizAppQuestionMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppQuestionMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
