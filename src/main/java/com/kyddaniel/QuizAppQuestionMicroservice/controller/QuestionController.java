f2b1d6b9721e01bbb3fe3a488d3bb85553ad5f47
kyddaniel <mksuenaa@connect.ust.hk>
2024-12-13 23:05:29 +0800

    Implement the methods for Question Service

--- src/main/java/com/kyddaniel/QuizAppQuestionMicroservice/controller/QuestionController.java
+++ src/main/java/com/kyddaniel/QuizAppQuestionMicroservice/controller/QuestionController.java
@@ -1,10 +1,13 @@
 package com.kyddaniel.QuizAppQuestionMicroservice.controller;
 
 import com.kyddaniel.QuizAppQuestionMicroservice.model.Question;
+import com.kyddaniel.QuizAppQuestionMicroservice.model.QuestionWrapper;
+import com.kyddaniel.QuizAppQuestionMicroservice.model.Response;
 import com.kyddaniel.QuizAppQuestionMicroservice.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.ls.LSException;
 
 import java.util.List;
 
@@ -28,6 +31,21 @@ public class QuestionController {
     @PostMapping("/add")
     public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
+    }
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String category, @RequestParam Integer numQuestions) {
+        return questionService.getQuestionsForQuiz(category, numQuestions);
+    }
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionsIDs) {
+        return questionService.getQuestionsFromId(questionsIDs);
+    }
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses) {
+        return questionService.getScore(responses);
     }
 
 }
--- src/main/java/com/kyddaniel/QuizAppQuestionMicroservice/service/QuestionService.java
+++ src/main/java/com/kyddaniel/QuizAppQuestionMicroservice/service/QuestionService.java
@@ -2,6 +2,8 @@ package com.kyddaniel.QuizAppQuestionMicroservice.service;
 
 import com.kyddaniel.QuizAppQuestionMicroservice.dao.QuestionDao;
 import com.kyddaniel.QuizAppQuestionMicroservice.model.Question;
+import com.kyddaniel.QuizAppQuestionMicroservice.model.QuestionWrapper;
+import com.kyddaniel.QuizAppQuestionMicroservice.model.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuestionService {
@@ -47,5 +50,34 @@ public class QuestionService {
         }
 
         return new ResponseEntity<>("not created", HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(String category, int numQuestions) {
+        List<Integer> questions = questionDao.findRandomQuestionsByCategory(category, numQuestions);
+        return new ResponseEntity<>(questions, HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionsIDs) {
+        List<QuestionWrapper> wrappers = new ArrayList<>();
+
+        for (int id : questionsIDs) {
+            Optional<Question> question = questionDao.findById(id);
+            question.ifPresent(value -> wrappers.add(new QuestionWrapper(value)));
+        }
+
+        return new ResponseEntity<>(wrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+
+        int score = 0;
+
+        for (Response response : responses) {
+            Optional<Question> question = questionDao.findById(response.getId());
+            if (question.isPresent() && response.getResponse().equals(question.get().getCorrectAnswer()))
+                score++;
+        }
+
+        return new ResponseEntity<>(score, HttpStatus.OK);
     }
 }
